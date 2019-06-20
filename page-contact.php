3bb8c50f0ac21bfabba0239ea8443d318c2a4cde
Matthew Kennett-Ny <matthewkennettny@gmail.com>
2019-06-20 19:07:05 +1200

    completed contact page and fixed nav colors

--- page-contact.php
+++ page-contact.php
@@ -1,53 +1,70 @@
 <?php get_header(); ?>
 
+<body>
+	<div class="container">
+		<div class="innerwrap">
+		
+			<section class="section1 clearfix">
+				<div class="textcenter">
+					<span class="shtext">Contact Us</span>
+					<span class="seperator"></span>
+					<h1>Drop Us a Mail</h1>
+				</div>
+			</section>
+		
+			<section class="section2 clearfix">
+				<div class="col2 column1 first">
+					<script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script><div class="sec2map" style='overflow:hidden;height:550px;width:100%;'><div id='gmap_canvas' style='height:100%;width:100%;'></div><div><small><a href="http://embedgooglemaps.com">									embed google maps							</a></small></div><div><small><a href="http://freedirectorysubmissionsites.com/">free web directories</a></small></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></div><script type='text/javascript'>function init_map(){var myOptions = {zoom:14,center:new google.maps.LatLng(19.075314480255834,72.88153973865361),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(19.075314480255834,72.88153973865361)});infowindow = new google.maps.InfoWindow({content:'<strong>My Location</strong><br>mumbai<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
+				</div>
+				<div class="col2 column2 last">
+					<div class="sec2innercont">
+						<div class="sec2addr">
+							<p>45 BC, a Latin professor at Hampden-Sydney College in Virginia</p>
+							<p><span class="collig">Phone :</span> +91 976885083</p>
+							<p><span class="collig">Email :</span> vivek.mengu016@gmail.com</p>
+							<p><span class="collig">Fax :</span> +91 9768850839</p>
+						</div>
+					</div>
+					<div class="sec2contactform">
+						<h3 class="sec2frmtitle">Want to Know More?? Drop Us a Mail</h3>
+						<form action="">
+							<div class="clearfix">
+								<input class="col2 first" type="text" placeholder="FirstName">
+								<input class="col2 last" type="text" placeholder="LastName">
+							</div>
+							<div class="clearfix">
+								<input  class="col2 first" type="Email" placeholder="Email">
+								<input class="col2 last" type="text" placeholder="Contact Number">
+							</div>
+							<div class="clearfix">
+								<textarea name="textarea" id="" cols="30" rows="7">Your message here...</textarea>
+							</div>
+							<div class="clearfix"><input type="submit" value="Send"></div>
+						</form>
+					</div>
 
-
-<div class="contactForm">  
-	<div class="map-responsive">
-		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2892.3895737596317!2d172.64102595853598!3d-43.53592055857888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d318a2280fe7491%3A0xfa4a25ea7d24f8c6!2sKennett+Crafted+Jewels!5e0!3m2!1sen!2snz!4v1560487929314!5m2!1sen!2snz" width="600" height="450" align="center" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+			</section>
+		
+		</div>
 	</div>
-    <div class="contactInformation">
-        <div class='contactqinfo'>
-            <h3>Contact</h3>
+</body>
+
+
+
+
+<!-- <div class='contactinfo'>
+          <div>   
             <p>P. 64 3 366 1232</p>
             <p>E. craftedjewels@kennett.net.nz</p>
             <p>Billens House,</p>
             <p>175 High Street,</p>
             <p>Christchurch 8011</p>
-        </div>
-        <div class='hours'>
+          </div>
+          <div class='hours'>
             <p>Mon-Fri 9.30am-5.00pm</p>
             <p>Sat 10.00am-3.00pm</p>
-        </div>    
-		</div>
-		
-  <form id="contact" action="" method="post">
-    <fieldset>
-      <input placeholder="Your name" type="text" tabindex="1" required autofocus>
-    </fieldset>
-    <fieldset>
-      <input placeholder="Your Email Address" type="email" tabindex="2" required>
-    </fieldset>
-    <fieldset>
-      <input placeholder="Your Phone Number (optional)" type="tel" tabindex="3" required>
-    </fieldset>
-    <fieldset>
-      <input placeholder="Your Web Site (optional)" type="url" tabindex="4" required>
-    </fieldset>
-    <fieldset>
-      <textarea placeholder="Type your message here...." tabindex="5" required></textarea>
-    </fieldset>
-    <fieldset>
-      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
-    </fieldset>
-	</form>
-	
-	
-</div>
-
-
-
-
+          </div> -->
 
 <?php
 get_footer(); ?>
